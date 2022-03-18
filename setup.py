7ea3cad7810283979c2d46cf7d84ccd7f59418d5
bonzoyang <bonzoyang@gmail.com>
2022-03-18 14:15:32 +0800

    v0.0.15

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="DSPutility",
-    version="0.0.14",
+    version="0.0.15",
     author="bonzo",
     author_email="bonzo.yang@dsp.im",
     description="DSP utility collection",
